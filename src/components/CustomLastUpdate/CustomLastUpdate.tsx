bcf3fff0ac3dcb3f17ab3b500da72fa02a0dd69b
Fedor Abrashin <abrashin012@gmail.com>
2024-12-25 16:00:02 +0300

    use locale

--- src/components/CustomLastUpdate/CustomLastUpdate.tsx
+++ src/components/CustomLastUpdate/CustomLastUpdate.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { translate } from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const NARROW_SPACE = '\u202F';
 
-export const CustomLastUpdate = ({ lastUpdatedAt }: { lastUpdatedAt: number }): JSX.Element => (
-  <div className="rounded-sm bg-blue-50 px-1.5 py-0.5 text-blue-500 text-xs font-normal w-fit mb-3">
-    <span>
-      {translate({ id: 'theme.lastUpdated.atDate', message: 'Обновлено:' })}
+export const CustomLastUpdate = ({ lastUpdatedAt }: { lastUpdatedAt: number }): JSX.Element => {
+  const { i18n } = useDocusaurusContext();
 
-      <time dateTime={new Date(lastUpdatedAt * 1000).toISOString()}>
-        {new Intl.DateTimeFormat('ru', {
-          day: 'numeric',
-          month: 'long',
-          timeZone: 'UTC',
-        }).format(lastUpdatedAt)}
+  return (
+    <div className="rounded-sm bg-blue-50 px-1.5 py-0.5 text-blue-500 text-xs font-normal w-fit mb-3">
+      <span>
+        {translate({ id: 'theme.lastUpdated.atDate', message: 'Обновлено:' })}
 
-        {NARROW_SPACE}
+        <time dateTime={new Date(lastUpdatedAt * 1000).toISOString()}>
+          {new Intl.DateTimeFormat(i18n.currentLocale, {
+            day: 'numeric',
+            month: 'long',
+            timeZone: 'UTC',
+          }).format(lastUpdatedAt)}
 
-        {new Intl.DateTimeFormat('ru', {
-          year: 'numeric',
-        }).format(lastUpdatedAt)}
-      </time>
-    </span>
-  </div>
-);
+          {NARROW_SPACE}
+
+          {new Intl.DateTimeFormat(i18n.currentLocale, {
+            year: 'numeric',
+          }).format(lastUpdatedAt)}
+        </time>
+      </span>
+    </div>
+  );
+};
