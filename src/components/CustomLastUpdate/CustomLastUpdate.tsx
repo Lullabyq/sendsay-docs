ef65c91422e26da1b2144dfbd61944fa67eb0559
Fedor Abrashin <abrashin012@gmail.com>
2024-12-25 16:25:00 +0300

    format date correctly

--- src/components/CustomLastUpdate/CustomLastUpdate.tsx
+++ src/components/CustomLastUpdate/CustomLastUpdate.tsx
@@ -6,24 +6,27 @@ const NARROW_SPACE = '\u202F';
 
 export const CustomLastUpdate = ({ lastUpdatedAt }: { lastUpdatedAt: number }): JSX.Element => {
   const { i18n } = useDocusaurusContext();
+  const isRuLocale = i18n.currentLocale === 'ru';
 
   return (
     <div className="rounded-sm bg-blue-50 px-1.5 py-0.5 text-blue-500 text-xs font-normal w-fit mb-3">
       <span>
-        {translate({ id: 'theme.lastUpdated.atDate', message: 'Обновлено:' })}
+        {translate({ id: 'lastUpdated.atDate', message: 'Обновлено: ' })}
 
         <time dateTime={new Date(lastUpdatedAt * 1000).toISOString()}>
           {new Intl.DateTimeFormat(i18n.currentLocale, {
             day: 'numeric',
             month: 'long',
+            year: isRuLocale ? undefined : 'numeric',
             timeZone: 'UTC',
           }).format(lastUpdatedAt)}
 
           {NARROW_SPACE}
 
-          {new Intl.DateTimeFormat(i18n.currentLocale, {
-            year: 'numeric',
-          }).format(lastUpdatedAt)}
+          {isRuLocale &&
+            new Intl.DateTimeFormat('ru', {
+              year: 'numeric',
+            }).format(lastUpdatedAt)}
         </time>
       </span>
     </div>
