4cbe1eb74b2ba83c60ab60d715d6e8367c4731b7
Fedor Abrashin <abrashin012@gmail.com>
2024-12-25 17:17:09 +0300

    fix wrong year date bug

--- src/components/CustomLastUpdate/CustomLastUpdate.tsx
+++ src/components/CustomLastUpdate/CustomLastUpdate.tsx
@@ -2,31 +2,33 @@ import React from 'react';
 import { translate } from '@docusaurus/Translate';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-const NARROW_SPACE = '\u202F';
+const NBSP = '\u00A0';
 
 export const CustomLastUpdate = ({ lastUpdatedAt }: { lastUpdatedAt: number }): JSX.Element => {
   const { i18n } = useDocusaurusContext();
+
   const isRuLocale = i18n.currentLocale === 'ru';
+  const formattedUpdatedAt = lastUpdatedAt * 1000;
 
   return (
     <div className="rounded-sm bg-blue-50 px-1.5 py-0.5 text-blue-500 text-xs font-normal w-fit mb-3">
       <span>
         {translate({ id: 'lastUpdated.atDate', message: 'Обновлено: ' })}
 
-        <time dateTime={new Date(lastUpdatedAt * 1000).toISOString()}>
+        <time dateTime={new Date(formattedUpdatedAt).toISOString()}>
           {new Intl.DateTimeFormat(i18n.currentLocale, {
             day: 'numeric',
             month: 'long',
             year: isRuLocale ? undefined : 'numeric',
             timeZone: 'UTC',
-          }).format(lastUpdatedAt)}
+          }).format(formattedUpdatedAt)}
 
-          {NARROW_SPACE}
+          {NBSP}
 
           {isRuLocale &&
             new Intl.DateTimeFormat('ru', {
               year: 'numeric',
-            }).format(lastUpdatedAt)}
+            }).format(formattedUpdatedAt)}
         </time>
       </span>
     </div>
